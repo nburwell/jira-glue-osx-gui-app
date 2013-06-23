d41c37e372a1069145e1801d3e4fcabef689cb1a
Nick Burwell <nburwell@gmail.com>
2013-06-22 21:52:25 -0700

    Auto add to clipboard (text and HTML versions) if checkbox set

--- JIRA Glue/AppDelegate.rb
+++ JIRA Glue/AppDelegate.rb
@@ -10,6 +10,8 @@ class AppDelegate
     attr_accessor :window
     attr_accessor :textFieldKey
     attr_accessor :textFieldOutput
+    attr_accessor :labelStatus
+    attr_accessor :checkboxClipboard
     
     attr_accessor :jira_client
     
@@ -20,8 +22,8 @@ class AppDelegate
     # TODO
     #  * put search on background thread
     #  * ideally do start-up on background thread too..
-    #  * put on clipboard
-    #  * put on clipboard as HTML
+    #  X put on clipboard
+    #  X put on clipboard as HTML
     
     JIRA_BASE_URL = "https://ringrevenue.atlassian.net"
     
@@ -38,22 +40,41 @@ class AppDelegate
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
         
-        textFieldOutput.setStringValue("Connecting to JIRA...")
         self.jira_client = JIRA::Client.new(JIRA_CLIENT_OPTIONS)
-        textFieldOutput.setStringValue("")
+        labelStatus.setStringValue("")
     end
     
     def onButtonClick(sender)
-        textFieldOutput.setStringValue("Searching for issue...")
-        issue = jira_client.Issue.find(textFieldKey.stringValue)
+        labelStatus.setStringValue("")
+        textFieldOutput.setStringValue("")
         
-        url = "#{JIRA_BASE_URL}/issues/#{issue.key}"
-        key = "#{issue.key}: #{issue.summary}"
-        textFieldOutput.setStringValue(key)
-    end
-    
-    def link_to_jql(jql)
-        "#{JIRA_BASE_URL}/issues/?jql=#{CGI.escape(jql)}"
+        if textFieldKey.stringValue == ""
+            labelStatus.setStringValue("Please enter a JIRA key")
+            NSLog("nothing to do")
+        else
+            labelStatus.setStringValue("Searching for issue...")
+            
+            begin
+                issue = jira_client.Issue.find(textFieldKey.stringValue)
+                
+                url = "#{JIRA_BASE_URL}/issues/#{issue.key}"
+                key = "#{issue.key}: #{issue.summary}"
+                textFieldOutput.setStringValue(key)
+                labelStatus.setStringValue("")
+                
+                if checkboxClipboard.state == NSOnState
+                    
+                    NSLog("copy to clipboard!")
+                    
+                    pasteBoard = NSPasteboard.generalPasteboard
+                    pasteBoard.declareTypes([NSHTMLPboardType, NSStringPboardType], owner: nil)
+                    pasteBoard.setString("<a href=\"#{url}\">#{issue.key}</a>: #{issue.summary}", forType: NSHTMLPboardType)
+                    pasteBoard.setString("#{key}", forType: NSStringPboardType)
+                end
+            rescue JIRA::HTTPError => ex
+                labelStatus.setStringValue("JIRA issue not found")
+            end
+        end
     end
 end
 
